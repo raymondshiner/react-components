59b61d90845ae15e4c93d3a3ab0aec81a60c0571
Raymond Shiner <raymondshiner@gmail.com>
2021-06-10 22:15:48 -0700

    0.1.6

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,11 @@
 import { Button } from "@material-ui/core";
-import { purple } from "@material-ui/core/colors";
 import { withStyles } from "@material-ui/styles";
 
-const StyledButton = withStyles((theme) => ({
-  root: {
-    color: "white",
-    backgroundColor: purple[500],
-    "&:hover": {
-      backgroundColor: purple[700],
-    },
-  },
-}))(Button);
+const StyledButton = withStyles((theme) => ({}))(Button);
 
 export default StyledButton;
+
+StyledButton.defaultProps = {
+  variant: "contained",
+  color: "primary",
+};
