99d31d679111dc273be08546dc250f56457db171
Raymond Shiner <raymondshiner@gmail.com>
2021-06-10 21:25:39 -0700

    rollup??

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import postcss from "rollup-plugin-postcss";
+
+const packageJson = require("./package.json");
+
+const rollup = {
+  input: "src/index.js",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), resolve(), commonjs(), postcss()],
+};
+
+export default rollup;
