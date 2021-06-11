faa0a51b2a3856d6370633f10eb2f60eb14c655e
Raymond Shiner <raymondshiner@gmail.com>
2021-06-10 22:11:48 -0700

    0.1.5

---
+++ src/themes.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const arcserveMuiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffeb3b",
+    },
+    secondary: {
+      main: "#9ccc64",
+    },
+  },
+});
