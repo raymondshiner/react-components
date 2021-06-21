a2da2512afe6c6c7e58de06cd6bd72f0e14c19bd
Raymond Shiner <raymondshiner@gmail.com>
2021-06-21 14:46:52 -0700

    theme changes

--- src/themes.js
+++ src/themes.js
@@ -1,12 +1,29 @@
 import { createMuiTheme } from "@material-ui/core";
 
 export const arcserveMuiTheme = createMuiTheme({
-  palette: {
+  pallete: {
     primary: {
-      main: "#ffeb3b",
+      main: '#6200EE',
+      dark: '#3700B3',
+      light: '#BB86FC',
+      background: '#6200EE',
+      border: '#6200EE',
+      contrastText: '#FFFFF',
     },
     secondary: {
-      main: "#9ccc64",
+      main: '#03DAC5',
+      dark: '#018786',
+      light: '#70EFDE',
+      background: '#03DAC5',
+      border: '#12DD99',
+      contrastText: '#000000',
     },
-  },
+    other: {
+      stroke: 'rgba(0,0,0,23)',
+      divider: "#E0E0E0",
+      backdrop: 'rgba(0,0,0,50)',
+      snackbar: "#323232",
+      activeRating: "#FFD500",
+    }
+  }
 });
